3b12498959a06a7ca090ca2cdf4305bffcdb9bf8
Alex <alex275221@gmail.com>
2025-08-06 21:26:56 -0300

    feat: agregar Vercel Analytics y actualizar los metadatos de la aplicación; crear robots.txt

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { Meta, Title } from '@angular/platform-browser';
+import { inject as injectVercelAnalytics } from '@vercel/analytics';  
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,19 @@ import { injectSpeedInsights } from '@vercel/speed-insights';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'personal-portfolio';
+  title = 'Alex.dev';
+  metaService = inject(Meta);
+  titleService = inject(Title);
+
   constructor() {
     injectSpeedInsights();
+    injectVercelAnalytics();
+    this.titleService.setTitle(this.title);
+
+     this.metaService.addTag({
+      name: 'description',
+      content: 'Alex.dev - Mi portafolio personal, donde comparto mis proyectos, habilidades y experiencias en desarrollo web.'
+    });
   }
   
 }
